0fd905829c2ac80b1a9ade2adfd7c34cce30d885
Praveen Kumar <praveen170596@gmail.com>
2022-11-15 11:53:52 +0530

    added toString method to convert the array to String

--- src/main/java/org/example/ArrayToString.java
+++ src/main/java/org/example/ArrayToString.java
@@ -7,10 +7,13 @@
 
 package org.example;
 
+import java.util.Arrays;
+
 public class ArrayToString {
     public static void main(String[] args) {
         //create the array
         int[] arrays = {111, 222, 333, 444, 555, 666, 777, 888, 999};
-
+        //convert the array to string using the toString method
+        System.out.println(Arrays.toString(arrays));
     }
 }
